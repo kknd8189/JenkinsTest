f91492040ec298ef04e9aa3076b1751af978c78a
kknd8189 <56291895+kknd8189@users.noreply.github.com>
2022-05-19 14:27:23 +0900

    33
    33

--- pipline_build_server.groovy
+++ pipline_build_server.groovy
@@ -1,12 +1,11 @@
+def RepositoryDir = 'Repository'
+def JenkinsScriptDir = 'JenkinsScriptDir'
+def JenkinsScriptGitURL = 'http://repositories.actionsquare.corp:3000/b2r/Tools.git'
 pipeline
 {
     parameters
     {
-        string defaultValue: 'http://repositories.actionsquare.corp/svn/b2r/trunk/BladeRA/Engine', description: '浚柳 家胶内靛 GIT 林家', name: 'ENGINE_SVN_URL', trim: false
-        string defaultValue: 'HEAD', description: 'Engine svn revision id', name: 'ENGINE_REVISION_ID', trim: false
-        string defaultValue: 'http://repositories.actionsquare.corp/svn/b2r/trunk/BladeRA/Content/BladeRA', description: '府家胶 SVN 林家', name: 'GAME_SVN_URL', trim: false
-        string defaultValue: 'HEAD', description: 'Game svn revision id', name: 'GAME_REVISION_ID', trim: false
-        string defaultValue: 'http://repositories.actionsquare.corp/svn/b2r/trunk/BladeRA/Client/Source', description: '霸烙 家胶内靛 SVN 林家', name: 'GAME_SRC_SVN_URL', trim: false
+        string defaultValue: 'http://repositories.actionsquare.corp/svn/b2r/trunk/BladeRA/Server/', description: '辑滚 SVN 林家', name: 'SEVER_SVN_URL', trim: false
         string defaultValue: 'HEAD', description: 'Game svn revision id', name: 'GAME_SRC_REVISION_ID', trim: false
         booleanParam defaultValue: false, description: '府呼靛咯何', name: 'REBUILD_ENGINE'
     }
@@ -22,21 +21,30 @@ pipeline
 
     stages 
     { 
-        stage('initial') 
-        {
-            steps 
-            {
-                echo 'Hello World'
-            }
+        stage('Server SVN CheckOut') 
+        {            
+                dir(RepositoryDir) {
+
+                    checkout([$class: 'SubversionSCM',
+                        additionalCredentials: [],
+                        excludedCommitMessages: '',
+                        excludedRegions: '',
+                        excludedRevprop: '',
+                        excludedUsers: '',
+                        filterChangelog: false,
+                        ignoreDirPropChanges: false,
+                        includedRegions: '',
+                        locations: [[cancelProcessOnExternalsFail: true, 
+                                        credentialsId: '_b2r_jenkins', 
+                                        depthOption: 'infinity', 
+                                        ignoreExternalsOption: false, 
+                                        local: '.', 
+                                        remote: params.ENGINE_SVN_URL + '@' + params.ENGINE_REVISION_ID]], 
+                                        quietOperation: true, 
+                                        workspaceUpdater: [$class: 'UpdateWithRevertUpdater']])
+
+                    bat 'echo Engine Git checkout - fininsh...'
+                }
         }
 
-        stage('stage1') 
-        {
-            steps 
-            {
-                echo 'Hello World'
-            }
-        }
-
-    }
 }
