ae91c47a9788396943c46ed5ca69207f554fc4b6
kknd8189 <56291895+kknd8189@users.noreply.github.com>
2022-05-20 09:46:00 +0900

    test

--- pipline_build_server_test.groovy
+++ pipline_build_server_test.groovy
@@ -5,40 +5,44 @@ def ServerDir = 'Server'
 
 pipeline
 {
-    parameters
-    {
-        string defaultValue: 'http://repositories.actionsquare.corp/svn/b2r/trunk/BladeRA/Server', description: '서버 SVN 주소', name: 'SERVER_SVN_URL', trim: false
-        string defaultValue: 'HEAD', description: 'Server svn revision id', name: 'SERVER_REVISION_ID', trim: false
-        booleanParam defaultValue: false, description: '리빌드여부', name: 'REBUILD_ENGINE'
-    }
+	parameters
+	{
+		string defaultValue: 'http://repositories.actionsquare.corp/svn/b2r/trunk/BladeRA/Server', description: '서버 SVN 주소', name: 'SERVER_SVN_URL', trim: false
+		string defaultValue: 'HEAD', description: 'Server svn revision id', name: 'SERVER_REVISION_ID', trim: false
+		booleanParam defaultValue: false, description: '리빌드여부', name: 'REBUILD_ENGINE'
+	}
 
-    agent
-    {
-        node
-        {
-            customWorkspace 'C:\\BladeRA'
-            label 'TEST_LABEL'
-        }
-    }
+	agent
+	{
+		node
+		{
+			customWorkspace 'C:\\BladeRA'
+			label 'TEST_LABEL'
+		}
+	}
 
 
-    stages 
-    { 
-        stage('Test CheckOut') 
-        {      
-            steps
-            {
-                  new File(RepositoryDir).mkdir()
-                  new File(ServerDir).mkdir()  
-            }
-        }
+	stages 
+	{ 
+		stage('Test CheckOut') 
+		{      
+			steps
+			{
+				script
+				{
+					new File(RepositoryDir).mkdir()
+					new File(ServerDir).mkdir()  
+				}
+			}
+		}
 
-        stage('Test Build')
-        {   
-            dir(ServerDir)
-            {
-                 checkout([$class: 'GitSCM', branches: [[name: 'main']], extensions: [], userRemoteConfigs: [[credentialsId: 'kknd8189', url: 'https://github.com/kknd8189/teamportfolio-SkotpillGrim.git']]])
-            }
-        }
-    }
+		stage('Test Build')
+		{   
+			dir(ServerDir)
+			{
+				checkout([$class: 'GitSCM', branches: [[name: 'main']], extensions: [], userRemoteConfigs: [[credentialsId: 'kknd8189', url: 'https://github.com/kknd8189/teamportfolio-SkotpillGrim.git']]])
+			}
+		}
+		
+	}
 }
